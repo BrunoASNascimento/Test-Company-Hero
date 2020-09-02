682483b056249259b4d31d5a2b2b7773b90c71ce
Bruno Nascimento <bruno.nascimento@pluvion.com.br>
2020-09-02 01:04:06 -0300

    add filter user

--- companyhero/hero/urls.py
+++ companyhero/hero/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
+from django.urls import include, path
+
 from . import views
 
 urlpatterns = [
     url(r'^employees/$', views.EmployeesList.as_view(), name='employee-list'),
+    path('username/<username>/', views.UsernameList.as_view(),
+         name='username-list')
 
 ]
--- companyhero/hero/views.py
+++ companyhero/hero/views.py
@@ -1,12 +1,22 @@
+from rest_framework.views import APIView
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Employees
 from .serializers import EmployeesSerializer
 
 # Create your views here.
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class EmployeesList(generics.ListCreateAPIView):
-
     queryset = Employees.objects.all()
     serializer_class = EmployeesSerializer
+
+
+class UsernameList(APIView):
+    def get(self, request, *args, **kwargs):
+        question = get_object_or_404(Employees, pk=kwargs['username'])
+        serializer = EmployeesSerializer(question)
+        return Response(serializer.data)
