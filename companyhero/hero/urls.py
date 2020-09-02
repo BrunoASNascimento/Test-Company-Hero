36b94cc0da54d9ef8af9adcd5e4b3d2dfba4407b
Bruno Nascimento <bruno.nascimento@pluvion.com.br>
2020-09-02 01:54:21 -0300

    filter enterprise done

--- companyhero/hero/urls.py
+++ companyhero/hero/urls.py
@@ -4,8 +4,10 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    url(r'^employees/$', views.EmployeesList.as_view(), name='employee-list'),
+    url(r'^employees/$', views.EmployeesCreate.as_view(), name='employee-list'),
     path('username/<username>/', views.UsernameList.as_view(),
-         name='username-list')
+         name='username-list'),
+    path('enterprise/<enterprise>', views.EmployeesList.as_view(),
+         name='enterprise-list'),
 
 ]
--- companyhero/hero/views.py
+++ companyhero/hero/views.py
@@ -7,10 +7,13 @@ from .serializers import EmployeesSerializer
 # Create your views here.
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
+from django.http import JsonResponse
 from rest_framework import status
+import json
+from django.core.serializers.json import DjangoJSONEncoder
 
 
-class EmployeesList(generics.ListCreateAPIView):
+class EmployeesCreate(generics.ListCreateAPIView):
     queryset = Employees.objects.all()
     serializer_class = EmployeesSerializer
 
@@ -18,5 +21,13 @@ class EmployeesList(generics.ListCreateAPIView):
 class UsernameList(APIView):
     def get(self, request, *args, **kwargs):
         question = get_object_or_404(Employees, pk=kwargs['username'])
-        serializer = EmployeesSerializer(question)
-        return Response(serializer.data)
+        serializer_class = EmployeesSerializer(question)
+        return Response(serializer_class.data)
+
+
+class EmployeesList(generics.ListCreateAPIView):
+    def get(self, request, *args, **kwargs):
+        queryset = Employees.objects.extra(
+            where=["'{}' = any (enterprise)".format(kwargs['enterprise'])])
+        serialized_q = json.dumps([dict(item) for item in queryset.values()])
+        return Response(json.loads(serialized_q))
